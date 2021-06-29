ef8c89789ed826daa4604fbfe527fa1be22a9b53
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-06-29 15:50:07 +0545

    connect to mongodb database

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose
+  .connect(process.env.DATABASE_URI, {
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+require("./db/db")
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
